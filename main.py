50383daea6fd3780bbe0f8ae0ebd2507535181d2
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-07-17 23:28:59 +0530

    Today Learned Get,Post Methods and Tested In RestFox API Testing Tool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,6 +8,17 @@ app = FastAPI()
 async def  read_root():
     return {"message":"Hello World"}
 
-@app.get("/greet/{name}")
-async def greet_name(name:str)->dict:
-    return {"message":f"Hello {name}"}
+@app.get("/greet")
+async def greet_name(name:Optional[str] = 'User' , age:int = 1)->dict:
+    return {"message":f"Hello {name} Your Age is {age}"}
+
+class Post(BaseModel):
+    gender:str
+    isPass:bool
+    
+@app.post('/post')
+async def post_this(post:Post):
+    return {
+        "Gender":post.gender,
+        "isPass":post.isPass
+    }
